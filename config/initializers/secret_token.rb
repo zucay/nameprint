1d437a4a9c847d782d1259838f9c257ab42efa48
yukihico <yukihico@gmail.com>
2012-01-02 10:39:05 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nameprint::Application.config.secret_token = '11b8fc99d650df5f05606d092f5f795b762aa98758d8fafaf8ab6d5d9fde1e80d77243929db3c5ec738fbcb50384455d5c5064339d0cf356919284c876187da8'
